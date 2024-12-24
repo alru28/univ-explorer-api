b1ea87767a026292b8dd3d1f5f686796df206b4b
Alvaro Ruiz <82512887+alru28@users.noreply.github.com>
2024-12-24 15:50:18 +0100

    Diff

--- src/celestial-collection/main.py
+++ src/celestial-collection/main.py
@@ -16,7 +16,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],  # Allows all origins
     allow_credentials=True,
-    allow_methods=["GET"],  # Allows GET method
+    allow_methods=["*"],  # Allows all methods
     allow_headers=["*"],  # Allows all headers
 )
 
