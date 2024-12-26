92b2822f2012b1fdca5de6dc55c6f3e26503c3cd
Alvaro Ruiz <82512887+alru28@users.noreply.github.com>
2024-12-26 11:39:45 +0100

    NavBar + Logo

--- src/react-frontend/src/components/Navbar.jsx
+++ src/react-frontend/src/components/Navbar.jsx
@@ -1,38 +1,47 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { NavLink } from 'react-router-dom';
 import {
   Navbar,
   NavbarBrand,
   Nav,
   NavItem,
+  Collapse,
+  NavbarToggler
 } from 'reactstrap';
 import logo from '../assets/logo.png';
 
-const AppNavbar = () => {
-  return (
-    <Navbar color="light" light expand="md" className="mb-4">
-      <NavbarBrand href="/collection">
-        <img src={logo} alt="Logo" style={{ height: '40px' }} />
-      </NavbarBrand>
-      <Nav className="ml-auto" navbar>
-        <NavItem>
-          <NavLink to="/collection" className="nav-link">
-            Collection
-          </NavLink>
-        </NavItem>
-        <NavItem>
-          <NavLink to="/explore" className="nav-link">
-            Explore
-          </NavLink>
-        </NavItem>
-        <NavItem>
-          <NavLink to="/profile" className="nav-link">
-            Profile
-          </NavLink>
-        </NavItem>
-      </Nav>
+const AppNavbar = (props) => {
+
+    const [collapsed, setCollapsed] = useState(true);
+    const toggleNavbar = () => setCollapsed(!collapsed);
+
+    return (
+    <Navbar color="dark" dark expand="md" className="mb-4">
+        <NavbarBrand href="/collection">
+        <img src={logo} alt="Logo" style={{ height: '60px' }} />
+        </NavbarBrand>
+        <NavbarToggler onClick={toggleNavbar} color="dark" dark className="me-2" />
+        <Collapse isOpen={!collapsed} navbar>
+            <Nav className="ml-auto" navbar>
+            <NavItem>
+                <NavLink to="/collection" className="nav-link">
+                Collection
+                </NavLink>
+            </NavItem>
+            <NavItem>
+                <NavLink to="/explore" className="nav-link">
+                Explore
+                </NavLink>
+            </NavItem>
+            <NavItem>
+                <NavLink to="/profile" className="nav-link">
+                Profile
+                </NavLink>
+            </NavItem>
+            </Nav>
+        </Collapse>
     </Navbar>
-  );
+    );
 };
 
 export default AppNavbar;
